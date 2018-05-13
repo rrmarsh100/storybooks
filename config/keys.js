8830197238201bea0d1d7a33cb84c5d48b152a8f
Robert Marsh <nhe6dc@gmail.com>
2018-05-13 12:28:15 -0400

    handled auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:'mongodb://bobby:bobby@ds121950.mlab.com:21950/storybooks-dev',
+  googleClientID:'267467914818-04keouaq44m8qcdjc3cgbiconhergqac.apps.googleusercontent.com',
+  googleClientSecret:'_ATeoWO5_WcVM6I17YtjJuWh'
+}
